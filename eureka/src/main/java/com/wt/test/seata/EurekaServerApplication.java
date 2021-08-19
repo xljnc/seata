32915dfbe7529b7a8de2088e53be927ce5773db3
yipin.wt <yipin.wt@cfuture.shop>
2021-08-19 15:39:19 +0800

    添加eureka server by qiyu 2021-08-19

---
+++ eureka/src/main/java/com/wt/test/seata/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.wt.test.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 一贫
+ * @date 2021/8/19
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
