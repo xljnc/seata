a5f13efb2cfa0694ac0bc51ca154d78b834a8f04
yipin.wt <yipin.wt@cfuture.shop>
2021-08-19 21:48:05 +0800

    添加feign by qiyu 2021-08-19

--- product/src/main/java/com/wt/test/seata/ProductApplication.java
+++ product/src/main/java/com/wt/test/seata/ProductApplication.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class StockApplication {
+public class ProductApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StockApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 }
